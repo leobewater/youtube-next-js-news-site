5f09e8b0789b10c15f9f208231a92365892e470e
Leo Cheung <leo.cheung@cbsinteractive.com>
2023-03-30 14:17:40 -0400

    populate articles from api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
+import { NewsArticle, NewsResponse } from '@/models/NewsArticles'
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-export default function BreakingNewsPage() {
+// Set up component props interface
+interface BreakingNewsPageProps {
+  newsArticles: NewsArticle[],
+}
+
+// Get data on server side, API key not going to be exposed
+export const getServerSideProps: GetServerSideProps<BreakingNewsPageProps> = async () => {
+  const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`);
+  const newsResponse: NewsResponse = await response.json();
+  return {
+    props: {
+      newsArticles: newsResponse.articles
+    }
+  }
+}
+
+// Set the prop and its type
+export default function BreakingNewsPage({ newsArticles }: BreakingNewsPageProps) {
   return (
     <>
       <Head>
@@ -9,6 +28,7 @@ export default function BreakingNewsPage() {
       </Head>
       <main>
         <h1>Breaking News</h1>
+        {JSON.stringify(newsArticles)}
       </main>
     </>
   )
