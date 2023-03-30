9ab7d4bb76fdf775588ecfe7311387ced538ca73
Leo Cheung <leo.cheung@cbsinteractive.com>
2023-03-30 15:16:45 -0400

    finish up client side search

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,6 +1,8 @@
+import NewsArticlesGrid from "@/components/NewsArticlesGrid";
 import { NewsArticle } from "@/models/NewsArticles";
+import Head from "next/head";
 import { FormEvent, useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Alert, Button, Form, Spinner } from "react-bootstrap";
 
 const SearchNewsPage = () => {
     // Set type to searchResults, it could be articles or null
@@ -18,21 +20,62 @@ const SearchNewsPage = () => {
 
         if (searchQuery) {
             // Set up a backend to handle the query search using the /pages/api
-            alert(searchQuery)
+            try {
+                // Reset previous search results
+                setSearchResults(null);
+                setSearchResultsLoadingIsError(false);
+                setSearchResultsLoading(true);
+
+                // Make request and set results to variable
+                const response = await fetch(`/api/search-news?q=${searchQuery}`)
+                const articles: NewsArticle[] = await response.json();
+                setSearchResults(articles);
+
+            } catch (error) {
+                console.error(error);
+                setSearchResultsLoadingIsError(true);
+            } finally {
+                setSearchResultsLoading(false);
+            }
         }
     }
 
     return (
-        <main>
-            <h1>Search News</h1>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="search-input">
-                    <Form.Label>Search query</Form.Label>
-                    <Form.Control name="searchQuery" placeholder="E.G. politics, sports, ..." />
-                </Form.Group>
-                <Button type="submit" className="mb-3" disabled={searchResultsLoading}>Search</Button>
-            </Form>
-        </main>
+        <>
+            {/* Override page title */}
+            <Head>
+                <title key="title">Search News - NextJS News App</title>
+            </Head>
+            <main>
+                <h1>Search News</h1>
+                
+                <Alert>
+                    This page uses client-side data fetching to show fresh data for every search. Requests are handled by our backend via API routes.
+                </Alert>
+
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="search-input">
+                        <Form.Label>Search query</Form.Label>
+                        <Form.Control name="searchQuery" placeholder="E.G. politics, sports, ..." />
+                    </Form.Group>
+                    <Button type="submit" className="mb-3" disabled={searchResultsLoading}>Search</Button>
+                </Form>
+
+                <div className="d-flex flex-column align-items-center">
+                    {/* When fetching data from api */}
+                    {searchResultsLoading && <Spinner animation="border" />}
+
+                    {/* Show error message */}
+                    {searchResultsLoadingIsError && <p>Something went wrong. Please try again.</p>}
+
+                    {/* Show 0 results */}
+                    {searchResults?.length === 0 && <p>Nothing found. Try a different query.</p>}
+
+                    {searchResults && <NewsArticlesGrid articles={searchResults} />}
+
+                </div>
+            </main>
+        </>
     );
 }
 
