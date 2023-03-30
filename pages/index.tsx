98c6d8a89d609acd776937566f5ffcdbc28f0238
Leo Cheung <leo.cheung@cbsinteractive.com>
2023-03-30 14:49:18 -0400

    add dummy delay

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { NewsArticle, NewsResponse } from '@/models/NewsArticles'
 import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Alert } from 'react-bootstrap'
 
 // Set up component props interface
 interface BreakingNewsPageProps {
@@ -12,6 +13,9 @@ interface BreakingNewsPageProps {
 
 // Get data on server side, API key not going to be exposed
 export const getServerSideProps: GetServerSideProps<BreakingNewsPageProps> = async () => {
+  // Delay
+  // await new Promise(r => setTimeout(r, 3000));
+
   const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`);
   const newsResponse: NewsResponse = await response.json();
   return {
@@ -30,6 +34,9 @@ export default function BreakingNewsPage({ newsArticles }: BreakingNewsPageProps
       </Head>
       <main>
         <h1>Breaking News</h1>
+        <Alert>
+          This page uses getServerSideProps to fetch data server-side on every request. This allows search engines to crawl the page content and improves SEO.
+        </Alert>
         <NewsArticlesGrid articles={newsArticles} />
       </main>
     </>
