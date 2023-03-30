f15afdf720f177d090cd315cb1c0d327334ea2f5
Leo Cheung <leo.cheung@cbsinteractive.com>
2023-03-30 15:25:40 -0400

    set up static pages for categories

---
+++ pages/categories/[category].tsx
@@ -0,0 +1,31 @@
+import NewsArticlesGrid from "@/components/NewsArticlesGrid";
+import { NewsArticle, NewsResponse } from "@/models/NewsArticles";
+import { GetStaticProps } from "next";
+
+interface CategoryNewsPageProps {
+    newsArticles: NewsArticle[]
+}
+
+// Get data at compile time using GetStaticProps
+export const getStaticProps: GetStaticProps<CategoryNewsPageProps> = async ({ params }) => {
+    const category = params?.category?.toString().trim();
+
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.NEWS_API_KEY}`);
+    const newsResponse: NewsResponse = await response.json();
+
+    return {
+        props: {
+            newsArticles: newsResponse.articles
+        }
+    }
+}
+
+const CategoryNewsPage = ({ newsArticles }: CategoryNewsPageProps) => {
+    return (<>
+        <main>
+            <NewsArticlesGrid articles={newsArticles} />
+        </main>
+    </>);
+}
+
+export default CategoryNewsPage;
