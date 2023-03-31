3e6219b2a5ac0eb972bc1b58ce5df960f43d214d
Leo Cheung <leo.cheung@cbsinteractive.com>
2023-03-31 13:57:25 -0400

    add emoji icon

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,7 +1,7 @@
 const ErrorPage = () => {
     return (
         <div>
-            <h1>Error!</h1>
+            <h1>Error 😵‍💫!</h1>
             <p>Looks like something went wrong. Please refresh the page or contact support!</p>
         </div>
     );
